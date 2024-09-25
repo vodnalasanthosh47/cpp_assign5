8ad5444dad75932ffdc83bba5162b47159f2a433
Santhosh Vodnala <Santhosh.Vodnala@iiitb.ac.in>
2024-09-25 13:08:10 +0530

    Separated classes into h and cpp files

---
+++ zoo_classes.cpp
@@ -0,0 +1,186 @@
+#include <bits/stdc++.h>
+#include "zoo_species.h"
+
+using namespace std;
+
+string diet_type_to_string(diet_type type) {
+    switch (type)
+    {
+    case herbivore:
+        return "herbivore";
+    case carnivore:
+        return "carnivore";
+    default:
+        return "";
+    }
+    
+}
+
+string size_type_to_string(size_type type) {
+    switch (type)
+    {
+    case small:
+        return "small";
+    case medium:
+        return "medium";
+    case large:
+        return "large";
+    default:
+        return "";
+    }
+}
+
+string bird_feed_type_to_string(bird_feed_type type) {
+    switch (type)
+    {
+    case grain:
+        return "grain";
+    case insect:
+        return "insect";
+    case fish:
+        return "fish";
+    default:
+        return "";
+    }
+}
+
+string aqua_feed_type_to_string(aqua_feed_type type) {
+    switch (type)
+    {
+    case fishfood:
+        return "fishfood";
+    case livefish:
+        return "livefish";
+    case plants:
+        return "plants";
+    default:
+        return "";
+    }
+}
+
+// Zoo_species methods
+Zoo_species::Zoo_species(string name, int count) : Species_name(name), Species_count(count) {}
+
+    
+void Zoo_species::Set_name(string name) {
+    Species_name = name;
+}
+
+void Zoo_species::Set_count(int count) {
+    Species_count = count;
+}
+
+int Zoo_species::Get_count() const{
+    return Species_count;
+}
+
+char Zoo_species::Get_category() const {
+    return category;
+}
+
+string Zoo_species::to_str() const {
+    return Species_name + " " + to_string(Species_count);
+}
+
+void Zoo_species::Print() const {
+    cout << Species_name << " " << Species_count << " ";
+}
+
+bool Zoo_species::Match(string name, char category) const {
+    return Species_name == name && this->category == category;
+}
+
+// Mammal methods
+Mammal::Mammal(string name, int count, diet_type type) : Zoo_species(name, count) {
+    Diet_type = type;
+    category = 'M';
+}
+
+void Mammal::Set_diet_type(diet_type type) {
+    Diet_type = type;
+}
+
+string Mammal::to_str() const{
+    return Zoo_species::to_str() + " " + diet_type_to_string(Diet_type);
+}
+
+void Mammal::Print() const {
+    Zoo_species::Print();
+    cout << diet_type_to_string(Diet_type) << endl;
+}
+
+bool Mammal::Match_special_attr(string special_attr) const{
+    return special_attr == diet_type_to_string(Diet_type);
+}
+
+
+// Reptile methods
+Reptile::Reptile(string name, int count, size_type type) : Zoo_species(name, count) {
+    Feed_size = type;
+    category = 'R';
+}
+
+void Reptile::Set_feed_size(size_type type) {
+    Feed_size = type;
+}
+
+string Reptile::to_str() const {
+    return Zoo_species::to_str() + " " + size_type_to_string(Feed_size);
+}
+
+void Reptile::Print() const {
+    Zoo_species::Print();
+    cout << size_type_to_string(Feed_size) << endl;
+}
+
+bool Reptile::Match_special_attr(string special_attr) const {
+    return special_attr == size_type_to_string(Feed_size);
+}
+
+
+// Bird methods
+Bird::Bird(string name, int count, bird_feed_type type) : Zoo_species(name, count) {
+    Bird_feed = type;
+    category = 'B';
+}
+
+void Bird::Set_bird_feed(bird_feed_type type) {
+    Bird_feed = type;
+}
+
+string Bird::to_str() const {
+    return Zoo_species::to_str() + " " + bird_feed_type_to_string(Bird_feed);
+}
+
+void Bird::Print() const {
+    Zoo_species::Print();
+    cout << bird_feed_type_to_string(Bird_feed) << endl;
+}
+
+bool Bird::Match_special_attr(string special_attr) const {
+    return special_attr == bird_feed_type_to_string(Bird_feed);
+}
+
+
+// Aquatic methods
+Aquatic::Aquatic(string name, int count, aqua_feed_type type) : Zoo_species(name, count) {
+    Aqua_feed = type;
+    category = 'Q';
+}
+
+void Aquatic::Set_aqua_feed(aqua_feed_type type) {
+    Aqua_feed = type;
+}
+
+string Aquatic::to_str() const {
+    return Zoo_species::to_str() + " " + aqua_feed_type_to_string(Aqua_feed);
+}
+
+void Aquatic::Print() const {
+    Zoo_species::Print();
+    cout << aqua_feed_type_to_string(Aqua_feed) << endl;
+}
+
+bool Aquatic::Match_special_attr(string special_attr) const {
+    return special_attr == aqua_feed_type_to_string(Aqua_feed);
+}
---
+++ zoo_species.h
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+enum diet_type {herbivore, carnivore};
+enum size_type {small, medium, large};
+enum bird_feed_type {grain, insect, fish};
+enum aqua_feed_type {fishfood, livefish, plants};
+
+class Zoo_species {
+protected:
+    string Species_name;
+    char category;
+    int Species_count;
+public:
+    Zoo_species(string name, int count);
+    
+    virtual ~Zoo_species() = default;
+    
+    void Set_name(string name);
+
+    void Set_count(int count);
+
+    int Get_count() const;
+
+    char Get_category() const;
+
+    virtual string to_str() const;
+
+    virtual void Print() const;
+
+    bool Match(string name, char category) const;
+
+    virtual bool Match_special_attr(string special_attr) const = 0;
+};
+
+class Mammal: public Zoo_species {
+private:
+    diet_type Diet_type;
+public:
+    Mammal(string name, int count, diet_type type);
+    
+    ~Mammal() = default;
+
+    void Set_diet_type(diet_type type);
+
+    string to_str() const;
+
+    void Print() const;
+
+    bool Match_special_attr(string special_attr) const;
+};
+
+class Reptile: public Zoo_species {
+private:
+    size_type Feed_size;
+public:
+    Reptile(string name, int count, size_type type);
+    
+    ~Reptile() = default;
+
+    void Set_feed_size(size_type type);
+
+    string to_str() const;
+    
+    void Print() const;
+
+    bool Match_special_attr(string special_attr) const;
+};
+
+class Bird: public Zoo_species {
+private:
+    bird_feed_type Bird_feed;
+public:
+    Bird(string name, int count, bird_feed_type type);
+    
+    ~Bird() = default;
+
+    void Set_bird_feed(bird_feed_type type);
+    
+    string to_str() const;
+    
+    void Print() const;
+
+    bool Match_special_attr(string special_attr) const;
+};
+
+class Aquatic: public Zoo_species {
+private:
+    aqua_feed_type Aqua_feed;
+public:
+    Aquatic(string name, int count, aqua_feed_type type);
+    
+    ~Aquatic() = default;
+
+    void Set_aqua_feed(aqua_feed_type type);
+    
+    string to_str() const;
+    
+    void Print() const;
+
+    bool Match_special_attr(string special_attr) const;
+};
+
+class Zoo_manager {
+private:
+    vector<Zoo_species*> zoo;
+    int num_mammals, num_reptiles, num_birds, num_aquatics;
+
+    void print_category(char category, int num_category) const;
+public:
+    Zoo_manager();
+
+    ~Zoo_manager();
+
+    void add_species(char category, string name, int count, string special_attr);
+
+    void delete_species(char category, string name, int count);
+
+    void show_species(char category) const;
+
+    void save_to_txt(ofstream& fout) const;
+};
